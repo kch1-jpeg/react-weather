ca78ba951f14fcb324183f23bac90f246d9b74e6
kch1-jpeg <116844422+kch1-jpeg@users.noreply.github.com>
2022-12-12 21:15:51 +0000

    add content

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-  function handleResponse(response) {
-    alert(`weather in berlin is ${response.data.temperature.current}`);
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setMessage(true);
+    setWeather({
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
   }
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=berlin&key=b89oa74t3f2022fff569a253b5f033b0";
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>hello</h2>;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b89oa74t3f2022fff569a253b5f033b0`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Type a city" onChange={changeCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (message) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind:{Math.round(weather.wind)} km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
